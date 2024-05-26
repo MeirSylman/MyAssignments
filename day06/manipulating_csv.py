6bdf6de99c6fac24398c6cb39bffda61cd0d0f64
MeirSylman <166698516+MeirSylman@users.noreply.github.com>
2024-05-26 12:44:20 +0300

    Create manipulating_csv.py

---
+++ day06/manipulating_csv.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import sys
+
+def compute_stat(path, filename='expression_data.csv'):
+    file_path = f"{path}/{filename}"
+    df = pd.read_csv(file_path)
+
+    # normalization:
+    for column in df.columns:
+        sum_column = df[column].sum()
+        df[column] = df[column] / sum_column
+
+    print('Data normalized.\n Calculating genes means statistics.')
+
+    gene_means = df.mean(axis=1)
+    variance = gene_means.var()
+    mean = gene_means.mean()
+
+    print(f"\nVariance of the gene means: {variance}")
+    print(f"\nMean of the gene means: {mean}")
+
+
+path = sys.argv[1]
+compute_stat(path)
+
