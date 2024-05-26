93938c0907789b0c78491af07dbb58ac29e2120d
MeirSylman <166698516+MeirSylman@users.noreply.github.com>
2024-05-26 13:18:16 +0300

    Update manipulating_csv.py

--- day06/manipulating_csv.py
+++ day06/manipulating_csv.py
@@ -25,15 +25,3 @@ path = sys.argv[1]
 process= compute_stat(path)
 
 
-def test_compute(result_variance,result_mean):
-    
-    expected_variance = 3.5688415084138224e-06
-    expected_mean = 0.01
-
-    observd_variance = result_variance
-    observd_mean = result_mean
-    
-    assert observd_variance == expected_variance
-    assert observd_mean ==expected_mean
-
-test_compute(process[0],process[1])
