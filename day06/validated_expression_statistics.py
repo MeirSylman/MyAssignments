42a1336b0fb9af1a104a7ef0f2395018affb2f80
MeirSylman <166698516+MeirSylman@users.noreply.github.com>
2024-05-27 10:38:34 +0300

    Update validated_expression_statistics.py

--- day06/validated_expression_statistics.py
+++ day06/validated_expression_statistics.py
@@ -3,13 +3,11 @@ from manipulating_csv.py import compute_stat
 
 
 def test_compute(result_variance, result_mean):
-    
-    path = 'https://raw.githubusercontent.com/MeirSylman/MyAssignments/main/day06/expression_data.csv'
+    path = "expression_data.csv"
     expected_variance = 3.5688415084138224e-06
     expected_mean = 0.01
 
     observd_variance = result_variance
-    
     observd_mean = result_mean
     
     variance, mean = compute_stat(path)
