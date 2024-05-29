6bb5be9984b5feb9602b5335902840d38d51eb5e
MeirSylman <166698516+MeirSylman@users.noreply.github.com>
2024-05-29 13:56:39 +0300

    Update test_expression_statistics.py

--- day06/test_expression_statistics.py
+++ day06/test_expression_statistics.py
@@ -1,16 +1,12 @@
-import pytest
 from manipulating_csv import compute_stat
 
 
-def test_compute(result_variance, result_mean):
+def test_compute_stat():
     path = "expression_data.csv"
     expected_variance = 3.5688415084138224e-06
     expected_mean = 0.01
 
-    observd_variance = result_variance
-    observd_mean = result_mean
-    
     variance, mean = compute_stat(path)
-    assert observd_variance == expected_variance
-    assert observd_mean == expected_mean
-    
+
+    assert variance == expected_variance, f"Expected variance: {expected_variance}, but got: {variance}"
+    assert mean == expected_mean, f"Expected mean: {expected_mean}, but got: {mean}"
